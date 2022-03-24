207cfce6c20d5b7043ef218ad27ef653e933d894
Vignesh S <95733152+Vsekar05@users.noreply.github.com>
2022-03-24 11:41:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import plotly.express as px
 import dash
 import dash_auth
 import dash_core_components as dcc
